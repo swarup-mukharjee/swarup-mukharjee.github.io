3c334b7c19b04d04c960011b70ab52da48cd4552
swarup-mukharjee <swarupmukharjee@outlook.com>
2025-05-02 12:48:51 +0530

    comment vectorbackground

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Projects from "@/components/projects"
 import Contact from "@/components/contact"
 import Navbar from "@/components/navbar"
 import { ParallaxProvider } from "@/components/parallax-provider"
-import VectorBackground from "@/components/vector-background"
+// import VectorBackground from "@/components/vector-background"
 
 export default function Home() {
   return (
     <ParallaxProvider>
       <main className="relative">
-        <VectorBackground color="#FF7A00" density={40} speed={0.3} />
+        {/* <VectorBackground color="#FF7A00" density={40} speed={0.3} /> */}
         <Navbar />
         <Hero />
         <About />
